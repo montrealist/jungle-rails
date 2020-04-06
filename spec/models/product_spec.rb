0c0108b614c6e6efe4f8fb56397c1242426049ee
Max <montrealist@users.noreply.github.com>
2020-04-06 15:28:49 -0400

    product spec file

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+    # validation tests/examples here
+    it "is valid with name"
+    it "is valid with price"
+    it "is valid with quantity"
+    it "is valid with category"
+  end
 end
