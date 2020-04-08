277ee01c072e98e8820a5f4955dd2818dfb4c0a9
Max <montrealist@users.noreply.github.com>
2020-04-08 18:36:06 -0400

    http auth using a concern

---
+++ app/controllers/concerns/http_auth_concern.rb
@@ -0,0 +1,15 @@
+# from here: https://medium.com/weareevermore/how-to-add-http-basic-authentication-to-your-rails-application-e4e4d5b958d9
+module HttpAuthConcern  
+    extend ActiveSupport::Concern
+    
+    included do
+        before_action :http_authenticate
+    end
+    
+    def http_authenticate
+        # return true unless Rails.env == 'production'
+        authenticate_or_request_with_http_basic do |username, password|
+            username == ENV["ADMIN_USERNAME"] && password == ENV["ADMIN_PASSWORD"]
+        end
+    end
+end
